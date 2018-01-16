5f28855da2a9b1f1ba1a5b6245d5c8bdceb768f2
Telahnus <quan.jordan@gmail.com>
2018-01-16 19:50:48 -0400

    day2

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    console.log("ready!");
+    setScale();
+});
+
+function setScale() {
+    var back = $(".background");
+    var img = $("#bkgimg");
+    var front = $(".foreground");
+    var frontHeight = getHeightOfChildren(front);
+    var imgHeight = img.height();
+    var imgWidth = img.width();
+    var bodyWidth = $("body").width();
+
+    var base = 5;
+    var ratio = frontHeight / imgHeight;
+    if (ratio < 1) ratio = 1;
+    var z = base * ratio;
+    var scale = z + ratio;
+
+    back.css("transform", "translateZ(-" + z + "px) scale(" + scale + ")");
+
+    console.log(ratio);
+    console.log($("body").width());
+    console.log(imgWidth);
+}
+
+
+function getHeightOfChildren(elem) {
+    var totalHeight = 0;
+    elem.children().each(function () {
+        totalHeight += $(this).height();
+    });
+    return totalHeight;
+}
